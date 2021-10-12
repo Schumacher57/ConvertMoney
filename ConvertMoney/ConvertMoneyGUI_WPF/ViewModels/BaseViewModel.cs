1ffd8e4f860e49f79c575145fb9be2fc714771bc
Schumacher57 <ivanchenko.i91@mail.ru>
2021-10-12 21:17:44 +0700

    My first step on WPF.
    Create markup controls...

---
+++ ConvertMoney/ConvertMoneyGUI_WPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace ConvertMoneyGUI_WPF.ViewModels
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged (string propertyName ="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ ConvertMoney/ConvertMoneyGUI_WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertMoneyGUI_WPF.ViewModels
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+        private string _synchronizedText;
+
+        public string SynchronizedText
+        {
+            get => _synchronizedText;
+            set
+            {
+                _synchronizedText = value;
+                OnPropertyChanged(nameof(SynchronizedText));
+            }
+        }
+
+    }
+}
